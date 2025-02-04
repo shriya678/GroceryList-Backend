0266aeb94965c6b74bd619388b384cc5d0b1647e
shriya678 <guptashriya035@gmail.com>
2025-02-04 20:10:15 +0530

    exported express

--- index.js
+++ index.js
@@ -28,8 +28,5 @@ async function main(){
 
 app.use(express.json());
 app.use("/api/",groceryItemRoutes);
-const port = process.env.PORT || 3000;
-
-app.listen(port, () =>{
-    console.log("server is listening on port 8080");
-})
+// Export Express API for Vercel
+export default app;
